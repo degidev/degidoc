90e62af90f5b1495f2e903aa3efc65c11ea86119
degidev <dlp.diegolipa@gmail.com>
2025-06-12 11:28:35 -0500

    feat: agregabdo doumentacion de vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,7 @@ export default defineConfig({
 				},
 				{
 					label: 'HTML 🦥',
+					collapsed: true,
 					items: [
 						{ label: 'Introducción a HTML', link: '/guides/html/introduction/' },
 						{ label: 'Estructura básica', link: '/guides/html/basic-structure/' },
@@ -72,6 +73,52 @@ export default defineConfig({
 						{ label: '18. Expresiones regulares', link: '/guides/javascript/regex/' },
 						{ label: '19. Manejo de errores', link: '/guides/javascript/error-handling/' },
 						{ label: '20. Fetch API', link: '/guides/javascript/fetch-api/' }
+					],
+				},
+				{
+					label:'Libreriasa',
+					collapsed: true,
+					items: [
+						{ label: 'tailwindcss', link: '/guides/libraries/tailwindcss/' },
+						{ label: 'bootstrap icons', link: '/guides/libraries/bootstrap-icons/' },
+						{ label: 'bootstrap', link: '/guides/libraries/bootstrap/' },
+						{ label: 'bulma', link: '/guides/libraries/bulma/' },
+						{ label: 'fontawesome', link: '/guides/libraries/fontawesome/' },
+						{ label: 'sweetalert', link: '/guides/libraries/sweetalert/' },
+						{ label: 'animations', link: '/guides/libraries/animations/' },
+						{ label: 'tailwindcss animations', link: '/guides/libraries/tailwindcss-animations/' },
+						{ label: 'plugin tailwindcss animations', link: '/guides/libraries/tailwindcss-animations-plugin/' },
+					],
+				},
+
+				{
+					label: 'Vue 3',
+					collapsed: true,
+					items: [
+						{ label: '1. Introducción a Vue 3', link: '/guides/vue/introduction/' },
+						{ label: '2. Composition API', link: '/guides/vue/composition-api/' },
+						{ label: '3. Estructura del Componente', link: '/guides/vue/component-structure/' },
+						{ label: '4. Reactividad', link: '/guides/vue/reactivity/' },
+						{ label: '5. Props y Eventos', link: '/guides/vue/props-events/' },
+						{ label: '6. Computadas y Watchers', link: '/guides/vue/computed-watchers/' },
+						{ label: '7. Ciclo de Vida', link: '/guides/vue/lifecycle/' },
+						{ label: '8. Métodos y Funciones', link: '/guides/vue/methods-functions/' },
+						{ label: '9. Renderizado Condicional', link: '/guides/vue/conditional-rendering/' },
+						{ label: '10. Eventos y Modificadores', link: '/guides/vue/events-modifiers/' },
+						{ label: '11. Formularios', link: '/guides/vue/forms/' },
+						{ label: '12. Componentes', link: '/guides/vue/components/' },
+						{ label: '13. Estilos', link: '/guides/vue/styling/' },
+						{ label: '14. Directivas Personalizadas', link: '/guides/vue/custom-directives/' },
+						{ label: '15. Composables', link: '/guides/vue/composables/' },
+						{ label: '16. Vue Router', link: '/guides/vue/router/' },
+						{ label: '17. Pinia', link: '/guides/vue/pinia/' },
+						{ label: '18. Manejo de APIs', link: '/guides/vue/api-handling/' },
+						{ label: '19. Transiciones', link: '/guides/vue/transitions/' },
+						{ label: '20. Comunicación entre Componentes', link: '/guides/vue/component-communication/' },
+						{ label: '21. Slots', link: '/guides/vue/slots/' },
+						{ label: '22. Props Avanzadas', link: '/guides/vue/advanced-props/' },
+						{ label: '23. Emits Avanzado', link: '/guides/vue/advanced-emits/' },
+						{ label: '24. Provide e Inject', link: '/guides/vue/provide-inject/' }
 					],
 				},
 				{
